22e28f540867bc045c3ffd4e66f9089ac3bd59d6
Andre <aaquijef@andrew.cmu.edu>
2017-12-10 00:01:00 -0500

    NA

---
+++ hello_aliens.R
@@ -0,0 +1,4 @@
+cat("My dear friends from Namek: I have the opportunity today to tell you that the exoplanets you are
+about to inhabit don't seem to be in the best conditions. Unless you don't depend on water, food
+and an appropriate temperature, I'd recommend not settling there. Feel free to pay us a visit at
+Pittsburgh, Pennsylvania, where you will be more than welcome. Ciao!")
